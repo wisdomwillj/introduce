5e6e6d2378c0ebc1f8960d26e790ba3af7e016d5
wisdomwillj <wisdomwillj@gmail.com>
2021-09-12 11:17:03 +0900

    html 수정 - categories 추가

--- project/urls.py
+++ project/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view()), # blog 첫페이지는 project/views.py 내 PostList 클래스로 처리
+    path('', views.PostList.as_view()), # 리스트 페이지
+    path('<int:pk>/', views.single_post_page),  # 상세페이지
 ]
